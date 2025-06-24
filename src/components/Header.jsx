783768987092c23b32d0d5d3655fc825195dfa04
karenwy <karenwy.dev@gmail.com>
2025-06-24 20:45:03 +0100

    change breakpoint for hamburger menu
    switch to hamburger menu for screen under 1024px instead of 768px

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -19,16 +19,16 @@ const Header = () => {
 
   return (
     <header className="fixed top-0 left-0 w-full h-20 flex items-center z-40 bg-gradient-to-b from-zinc-900 to-zinc-900/0">
-      <div className="max-w-screen-2xl w-full mx-auto px-4 flex justify-between items-center md:px-6 md:grid md:grid-cols-[1fr_3fr_1fr]">
+      <div className="max-w-screen-2xl w-full mx-auto px-4 flex justify-between items-center lg:px-6 lg:grid lg:grid-cols-[1fr_3fr_1fr]">
         <h1>
           <a href="/" className="logo">
             {/* <img src="/images/icon-bg-black.png" width={40} height={40} alt="Karen Lee"></img> */}
             &lt; karen lee &gt;
           </a>
         </h1>
-        <div className="relative md:justify-self-center">
+        <div className="relative lg:justify-self-center">
           {/* On click toggle between states (open and close navbar) */}
-          <button className="menu-btn md:hidden!" onClick={() => setNavOpen((prev) => !prev)}>
+          <button className="menu-btn lg:hidden!" onClick={() => setNavOpen((prev) => !prev)}>
             <span className="material-symbols-rounded">
               {/* switch between close and menu icons when navbar open and closed */}
               {navOpen ? 'close' : 'menu'}
@@ -36,7 +36,7 @@ const Header = () => {
           </button>
           <Navbar NavOpen={navOpen} />
         </div>
-        <a href='#contact' className="btn btn-secondary max-md:hidden! md:justify-self-end">
+        <a href='#contact' className="btn btn-secondary max-lg:hidden! lg:justify-self-end">
           Contact Me
         </a>
       </div>
